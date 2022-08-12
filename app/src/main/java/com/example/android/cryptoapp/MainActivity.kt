fc733a86eca166749ee4dd92a260199b7f96393e
AmarantBCL <vadbcl@outlook.com>
2022-08-12 20:48:31 +0300

    Retrofit API

--- app/src/main/java/com/example/android/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/android/cryptoapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.android.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.android.cryptoapp.api.ApiFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.tv_test)
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                textView.text = it.toString()
+            }, {
+                Log.d("MY_CUSOM_TAG", it.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }

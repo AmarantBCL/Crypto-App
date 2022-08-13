0db3ed14d80d56845f86979ce45cb6ae79af523b
AmarantBCL <vadbcl@outlook.com>
2022-08-13 17:18:50 +0300

    Database & DAO

--- app/src/main/java/com/example/android/cryptoapp/database/AppDatabase.kt
+++ app/src/main/java/com/example/android/cryptoapp/database/AppDatabase.kt
@@ -1,19 +1,25 @@
 package com.example.android.cryptoapp.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [AppDatabase::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
         private const val DB_NAME = "main.db"
         private var db: AppDatabase? = null
+        private val LOCK = Any()
 
         fun getInstance(context: Context): AppDatabase {
-            db?.let { return it }
-            val instance = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
-            db = instance
-            return instance
+            synchronized(LOCK) {
+                db?.let { return it }
+                val instance =
+                    Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+                db = instance
+                return instance
+            }
         }
     }
 

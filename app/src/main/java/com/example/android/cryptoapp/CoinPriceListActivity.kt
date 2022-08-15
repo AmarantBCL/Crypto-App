130f3a16c28ed5f32480145e37c00eb67c77da19
AmarantBCL <vadbcl@outlook.com>
2022-08-15 15:33:38 +0300

    Views & utils

--- app/src/main/java/com/example/android/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/android/cryptoapp/CoinPriceListActivity.kt
@@ -4,29 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.android.cryptoapp.api.ApiFactory
 import com.example.android.cryptoapp.viewmodel.CoinViewModel
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
 
-class MainActivity : AppCompatActivity() {
+class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
-    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.tv_test)
+        setContentView(R.layout.activity_coin_price_list)
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.priceList.observe(this) {
             Log.d("MY_CUSTOM_TAG", "Success in activity: $it")
         }
         viewModel.getDetailedInfo("BTC").observe(this) {
-            textView.text = it.toString()
+
         }
     }
 }

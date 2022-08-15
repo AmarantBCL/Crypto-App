a93a41da96c4f70b7a9dfd0b8b5a06dc7d4af03a
AmarantBCL <vadbcl@outlook.com>
2022-08-15 14:57:51 +0300

    Finished view model

--- app/src/main/java/com/example/android/cryptoapp/database/AppDatabase.kt
+++ app/src/main/java/com/example/android/cryptoapp/database/AppDatabase.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.android.cryptoapp.pojo.CoinPriceInfo
 
-@Database(entities = [AppDatabase::class], version = 1, exportSchema = false)
+@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
         private const val DB_NAME = "main.db"

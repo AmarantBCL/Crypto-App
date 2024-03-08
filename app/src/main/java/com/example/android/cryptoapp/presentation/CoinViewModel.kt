f6b211446fbdaec6acc70b73f9ca540e873fe152
vpetel127 <vpetel127@cable.comcast.com>
2024-03-08 07:43:02 -0500

    WorkManager

--- app/src/main/java/com/example/android/cryptoapp/presentation/CoinViewModel.kt
+++ app/src/main/java/com/example/android/cryptoapp/presentation/CoinViewModel.kt
@@ -1,23 +1,11 @@
 package com.example.android.cryptoapp.presentation
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
-import com.example.android.cryptoapp.data.network.ApiFactory
-import com.example.android.cryptoapp.data.database.AppDatabase
-import com.example.android.cryptoapp.data.network.model.CoinInfoDto
-import com.example.android.cryptoapp.data.network.model.CoinInfoJsonContainerDto
 import com.example.android.cryptoapp.data.repository.CoinRepositoryImpl
 import com.example.android.cryptoapp.domain.GetCoinInfoListUseCase
 import com.example.android.cryptoapp.domain.GetCoinInfoUseCase
 import com.example.android.cryptoapp.domain.LoadDataUseCase
-import com.google.gson.Gson
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -32,8 +20,6 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
     fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
 
     init {
-        viewModelScope.launch {
-            loadDataUseCase()
-        }
+        loadDataUseCase()
     }
 }
